308108f26f7af3bba35846ac88f7a4b5f16fa352
S. V. Paulauskas <spaulaus@users.noreply.github.com>
2017-10-24 16:18:30 -0400

    Update .travis-script.sh

--- .travis-script.sh
+++ .travis-script.sh
@@ -3,8 +3,7 @@ set -ex
 
 SRC_DIR=$(dirname "$0")
 
-export CC=gcc-5 && export CXX=g++-5 && export FC=gfortran-5
-
-cmake $SRC_DIR/paass-laughing-conqueror -DPAASS_BUILD_SCAN_UTILITIES=ON -DPAASS_BUILD_ACQ=ON -DPAASS_BUILD_TESTS=ON -DPAASS_BUILD_UTKSCAN=ON -DPAASS_USE_ROOT=ON
+mkdir /tmp/build && cd /tmp/build
+cmake $SRC_DIR/paass-laughing-conqueror -DPAASS_BUILD_SCAN_UTILITIES=ON -DPAASS_BUILD_ACQ=ON -DPAASS_BUILD_TESTS=ON -DPAASS_BUILD_UTKSCAN=ON -DPAASS_USE_ROOT=ON -DCMAKE_INSTALL_PREFIX=/tmp/build/install
 make -j8 install
-for test in ../install/bin/unittests/*; do ./$test; if [ $? != 0 ]; then false; exit; fi; done
+for test in ./install/bin/unittests/*; do ./$test; if [ $? != 0 ]; then false; exit; fi; done
